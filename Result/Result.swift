b235911516a58898288762ed3ff27f688f9d8ed6
John Grange <john.grange.ny@gmail.com>
2015-08-07 18:32:21 -0700

    - Completed Result class
    Signed-off-by: John Grange <john.grange.ny@gmail.com>

--- Result/Result.swift
+++ Result/Result.swift
@@ -5,9 +5,15 @@
 //  Created by John Grange on 8/7/15.
 //  Copyright © 2015 SD Networks. All rights reserved.
 //
-
+//  
 import Foundation
 
+/**
+Simple ResultType enum, we will build slowly on this to keep things CLEAR and easy to read.
+
+- Success: Success case that will hold a value of any type
+- Failure: Failure case that will hold an error constrainted to type ErrorType
+*/
 public enum Result<T, Error: ErrorType> {
     
     case Success(T)
@@ -34,11 +40,38 @@ public enum Result<T, Error: ErrorType> {
             return error
         }
     }
-}
-
-//MARK: - Extend result to convert to a throwable error
-extension Result {
     
+    /**
+    Construct a result from a function that throws an error, useful in completion blocks
+    
+    i.e. Result(try NSData(contentsOfFile: "data.bin", options: []))
+    
+    :param: f function to do a try catch on
+    
+    :returns: a Result with either a failure or value of type returned by the function
+    */
+    public init(@autoclosure _ f: () throws -> T) {
+        do {
+            self = .Success(try f())
+        } catch {
+            self = .Failure(error as! Error)
+        }
+    }
+    
+    /**
+    Convert Result to a throwable function
+    
+    Exampe:
+    
+    do {
+        let json = try jsonResult.throwit()
+    } catch {
+        print(error)
+    }
+    
+    :returns: a function that you can use in a try / catch block
+    
+    */
     func throwit() throws -> T {
         
         switch self {
@@ -49,4 +82,20 @@ extension Result {
             throw error
         }
     }
+    
+    
+
 }
+infix operator ?! {
+    associativity right
+    precedence 131
+}
+
+func ?! <T>(optional: T?, @autoclosure error: () -> ErrorType) throws -> T {
+    if let value = optional {
+        return value
+    } else {
+        throw error()
+    }
+}
+
